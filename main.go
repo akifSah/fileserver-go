47f4d5f2fcc98368192b48e09d9920210d015ef9
akif-s <04makifsahin@gmail.com>
2023-01-09 17:31:11 +0300

    corrected mistype

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	port := flag.String("p", "8080", "Port to serve on.")
+	port := flag.String("p", ":8080", "Port to serve on.")
 	dir := flag.String("d", "./tmp", "the directory of static file to host")
 	flag.Parse()
 
