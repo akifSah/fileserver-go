0894cd8816beeb76c44f9bf10523f7665ed15548
akif-s <04makifsahin@gmail.com>
2023-01-09 17:27:12 +0300

    made './tmp' as default directory

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	port := flag.String("p", "8080", "Port to serve on.")
-	dir := flag.String("d", ".", "the directory of static file to host")
+	dir := flag.String("d", "./tmp", "the directory of static file to host")
 	flag.Parse()
 
 	fs := http.FileServer(http.Dir(*dir))
