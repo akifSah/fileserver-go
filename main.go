b5b3f3287cd3d6105378e672cd384f2dcfb900cf
akif-s <04makifsahin@gmail.com>
2023-01-09 19:29:26 +0300

    made dir relative to current working directory

--- main.go
+++ main.go
@@ -5,16 +5,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
+	pwd, _ := os.Getwd()
+
 	port := flag.String("p", ":8080", "Port to serve on.")
 	dir := flag.String("d", "./tmp", "the directory of static file to host")
 	flag.Parse()
 
-	fs := http.FileServer(http.Dir(*dir))
+	fs := http.FileServer(http.Dir(filepath.FromSlash(filepath.Join(pwd, *dir))))
 
-	fmt.Printf("Serving '%v' at %v", *dir, *port)
+	fmt.Printf("Serving '%v' at %v", filepath.Join(pwd, *dir), *port)
 	log.Fatal(http.ListenAndServe(*port, fs))
 
 }
